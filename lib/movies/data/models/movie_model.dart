cdb1bbfbc1e16349e355364906ef9f20808c2d89
Ahmed khaled <ahmedkh8907@gmail.com>
2024-06-15 08:15:02 +0300

    Data layer: Movie Model

---
+++ lib/movies/data/models/movie_model.dart
@@ -0,0 +1,25 @@
+import 'package:movies_app/movies/domain/entities/movie_entity.dart';
+
+class MovieModel extends MovieEntity {
+  const MovieModel({
+    required super.id,
+    required super.title,
+    required super.backdropPath,
+    required super.genreIds,
+    required super.overview,
+    required super.voteAverage,
+    required super.releaseDate,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
+    return MovieModel(
+      id: json['id'],
+      title: json['title'],
+      backdropPath: json['backdropPath'],
+      genreIds: List<int>.from(json['genreIds'].map((e) => e)),
+      overview: json['overview'],
+      voteAverage: json['vote_average'],
+      releaseDate: json['release_date'],
+    );
+  }
+}
